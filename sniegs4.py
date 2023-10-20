74fcce399b5de0c97619f15ae258a2f8d7447a9c
rezijaliepina <rezija.liepina@grobinasskola.lv>
2023-10-20 10:20:52 +0300

    sniegs4

---
+++ sniegs4.py
@@ -0,0 +1,11 @@
+import random
+# Ģenerē 100 skaitļus un ievieto tos terminālī
+
+
+
+skaitlu_saraksts = [random.randint(1, 100) for _ in range(100)]
+
+# Izdrukā skaitļus terminālī
+
+print("Ģenerētie skaitļi:")
+print(skaitlu_saraksts)
